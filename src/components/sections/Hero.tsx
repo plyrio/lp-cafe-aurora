28db6b19ec92c55c4782082196c6586da7d0c185
Pedro Lyrio <pedrolyrio@ucl.br>
2025-11-29 13:12:02 -0300

    feat: ajusta espaçamento e padding na seção Hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -7,8 +7,8 @@ export const Hero = () => {
       <div className="absolute inset-0 bg-hero bg-cover bg-center"></div>
       <div className="absolute inset-0 bg-linear-to-r from-black/60 via-black/20 to-transparent"></div>
 
-      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 md:px-8 item-center pt-20">
-        <div className="flex flex-col font-medium items-start max-w-3xl space-y-4 text-white">
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 md:px-8 item-center pt-16 pb-4">
+        <div className="flex flex-col font-medium items-start max-w-3xl space-y-4 text-white ">
 
           <p className="font-medium text-sm sm:text-base md:text-lg lg:text-xl">
             Nós garantimos o melhor começo para a sua manhã com
