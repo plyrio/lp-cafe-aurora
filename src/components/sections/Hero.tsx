d1001496e0571fe68b09512ca8ea49585d55d03e
Pedro Lyrio <pedrolyrio@ucl.br>
2025-11-29 21:04:48 -0300

    feat: adiciona framer-motion para animações na seção Hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react"
 import { Button } from "../ui/Button"
 import { Section } from "../ui/Section"
 
@@ -7,21 +8,42 @@ export const Hero = () => {
       <div className="absolute inset-0 bg-hero bg-cover bg-center"></div>
       <div className="absolute inset-0 bg-linear-to-r from-black/60 via-black/20 to-transparent"></div>
 
-      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 md:px-8 item-center pt-26  pb-14">
-        <div className="flex flex-col font-medium items-start max-w-3xl space-y-4 text-white ">
+      <div className="relative z-10 w-full max-w-7xl mx-auto px-4 md:px-8 item-center pt-26 pb-14">
+        <div className="flex flex-col font-medium items-start max-w-3xl space-y-4 text-white">
 
-          <p className="font-medium text-sm sm:text-base md:text-lg lg:text-xl">
+          <motion.p
+            initial={{ opacity: 0, x: -40 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.5, delay: 1.0 }}
+            className="font-medium text-sm sm:text-base md:text-lg lg:text-xl">
             Nós garantimos o melhor começo para a sua manhã com
-          </p>
-          <p className="font-brand leading-none text-[clamp(3rem,12vw,14rem)]">
+          </motion.p>
+
+
+          <motion.p
+            initial={{ opacity: 0, x: -40 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.6 }}
+            className="font-brand leading-none text-[clamp(3rem,12vw,14rem)]">
             Café
-          </p>
-          <p className="font-medium text-sm sm:text-base md:text-lg lg:text-xl max-w-xl">
+          </motion.p>
+
+          <motion.p
+            initial={{ opacity: 0, x: -40 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.6, delay: 1.2 }}
+            className="font-medium text-sm sm:text-base md:text-lg lg:text-xl max-w-xl">
             Nada melhor do que começar o dia com uma xícara de café.
             Descubra os melhores sabores que você já experimentou.
             Nós oferecemos sempre o melhor para nossos clientes.
-          </p>
-          <Button href={"#"}>Peça Agora</Button>
+          </motion.p>
+
+          <motion.div
+            initial={{ opacity: 0, x: -40 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1.6, delay: 1.6 }}>
+            <Button href="#">Peça Agora</Button>
+          </motion.div>
         </div>
       </div>
 
